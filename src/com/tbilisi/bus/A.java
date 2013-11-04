fdccb1a5b59c12b77b8a6c406b789462d0e2435b
Nick Shvelidze <captain@pirrate.me>
2013-11-04 23:13:23 +0400

    Super optimize DB

--- src/com/tbilisi/bus/A.java
+++ src/com/tbilisi/bus/A.java
@@ -10,19 +10,10 @@ import android.hardware.Camera;
 import android.os.AsyncTask;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-
-import com.tbilisi.bus.data.BusStop;
 import com.tbilisi.bus.data.DatabaseHelper;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.concurrent.Callable;
-import java.util.regex.Pattern;
 
 public class A extends Application implements Thread.UncaughtExceptionHandler {
     public static Context mContext;
@@ -59,8 +50,6 @@ public class A extends Application implements Thread.UncaughtExceptionHandler {
     }
 
     private class BaseLoader extends AsyncTask<Void,Integer,Void> {
-        private Elements stops;
-        private boolean clean = true;
         NotificationManager notificationManager;
 
         @Override
@@ -81,52 +70,65 @@ public class A extends Application implements Thread.UncaughtExceptionHandler {
                         .setOngoing(true);
                 notificationManager.notify(0, builder.build());
 
-                log("Parsing stops.xml");
-
-                Document document = Jsoup.parse(getAssets().open("stops.xml"), "UTF-8", "");
-                stops = document.select("Stops");
-
-                log("Parsed stops.xml");
-
                 db.busStopDao.callBatchTasks(new Callable<Void>() {
                     @Override
                     public Void call() throws Exception {
-                        String tagName; int id = -1; String name = ""; boolean hasBoard = false;
-                        boolean hasData = false; double lat = 0.0; double lon = 0.0;
-                        Pattern p = Pattern.compile(" - ");
-                        for(Element stop : stops) {
-                            if(! stop.select("Type").first().text().equals("bus")) continue;
-                            for(Element child: stop.children()) {
-                                tagName = child.tagName();
-                                if(tagName.equals("stopid")) {
-                                    id = Integer.valueOf(child.text());
-                                } else if(tagName.equals("name")) {
-                                    name = child.text();
-                                    name = p.split(name)[0];
-                                } else if(tagName.equals("hasboard")) {
-                                    hasBoard = Boolean.valueOf(child.text());
-                                } else if(tagName.equals("virtual")) {
-                                    hasData = Boolean.valueOf(child.text());
-                                } else if(tagName.equals("lat")) {
-                                    lat = Double.valueOf(child.text());
-                                } else if(tagName.equals("lon")) {
-                                    lon = Double.valueOf(child.text());
-                                }
-                            }
-                            if(name.length() == 0 || id == -1) continue;
-                            db.busStopDao.create(new BusStop(id, name, hasBoard, hasData, lat, lon));
+                        BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(getAssets().open("db.sql")));
+                        String line = reader.readLine();
+                        while (line != null) {
+                            db.busStopDao.executeRawNoArgs(line);
+                            line = reader.readLine();
                         }
+                        reader.close();
                         return null;
                     }
                 });
-                if(clean) {
-                    log("Operation successful");
-                } else {
-                    log("Operation failed");
-                }
+//                log("Parsing stops.xml");
+//
+//                Document document = Jsoup.parse(getAssets().open("stops.xml"), "UTF-8", "");
+//                stops = document.select("Stops");
+//
+//                log("Parsed stops.xml");
+//
+//                db.busStopDao.callBatchTasks(new Callable<Void>() {
+//                    @Override
+//                    public Void call() throws Exception {
+//                        String tagName; int id = -1; String name = ""; boolean hasBoard = false;
+//                        boolean hasData = false; double lat = 0.0; double lon = 0.0;
+//                        Pattern p = Pattern.compile(" - ");
+//                        for(Element stop : stops) {
+//                            if(! stop.select("Type").first().text().equals("bus")) continue;
+//                            for(Element child: stop.children()) {
+//                                tagName = child.tagName();
+//                                if(tagName.equals("stopid")) {
+//                                    id = Integer.valueOf(child.text());
+//                                } else if(tagName.equals("name")) {
+//                                    name = child.text();
+//                                    name = p.split(name)[0];
+//                                } else if(tagName.equals("hasboard")) {
+//                                    hasBoard = Boolean.valueOf(child.text());
+//                                } else if(tagName.equals("virtual")) {
+//                                    hasData = Boolean.valueOf(child.text());
+//                                } else if(tagName.equals("lat")) {
+//                                    lat = Double.valueOf(child.text());
+//                                } else if(tagName.equals("lon")) {
+//                                    lon = Double.valueOf(child.text());
+//                                }
+//                            }
+//                            if(name.length() == 0 || id == -1) continue;
+//                            db.busStopDao.create(new BusStop(id, name, hasBoard, hasData, lat, lon));
+//                        }
+//                        return null;
+//                    }
+//                });
+//                if(clean) {
+//                    log("Operation successful");
+//                } else {
+//                    log("Operation failed");
+//                }
             } catch (Exception e) {
                 e.printStackTrace();
-                clean = false;
             }
             return null;
         }
