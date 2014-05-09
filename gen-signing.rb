a891fa940a40f2cf117dbbb032c09709feb9d2c8
Nick Shvelidze <captain@pirrate.me>
2014-05-09 16:47:39 +0400

    Signing gen

---
+++ gen-signing.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+print "Keystore path: "
+keystore_path = gets.chomp
+
+print "Keystore password: "
+keystore_password = gets.chomp
+
+print "Key alias: "
+key_alias = gets.chomp
+
+print "Key password: "
+key_password = gets.chomp
+
+contents = <<eos
+android {
+    signingConfigs {
+        release {
+            storeFile file("#{keystore_path}")
+            storePassword "#{keystore_password}"
+            keyAlias "#{key_alias}"
+            keyPassword "#{key_password}"
+        }
+    }
+}
+eos
+
+puts contents
