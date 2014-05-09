4f815a93e18853604eaef6ab90f8f1f6d215b617
Nick Shvelidze <captain@pirrate.me>
2014-05-09 17:04:28 +0400

    Map activity

--- src/com/tbilisi/bus/MapActivity.java
+++ src/com/tbilisi/bus/MapActivity.java
@@ -1,6 +1,10 @@
 package com.tbilisi.bus;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 public class MapActivity extends ActionBarActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
