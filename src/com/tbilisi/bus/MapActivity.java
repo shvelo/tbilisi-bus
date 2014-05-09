f600c07bc625490195ad2e87cf222b3059713b03
Nick Shvelidze <captain@pirrate.me>
2014-05-09 17:03:46 +0400

    Add map activity

--- src/com/tbilisi/bus/MapActivity.java
+++ src/com/tbilisi/bus/MapActivity.java
@@ -1,7 +1,6 @@
 package com.tbilisi.bus;
 
-/**
- * Created by nick on 5/9/14.
- */
-public class MapActivity {
+import android.support.v7.app.ActionBarActivity;
+
+public class MapActivity extends ActionBarActivity {
 }
