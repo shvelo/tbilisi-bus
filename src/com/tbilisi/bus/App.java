f0c1a9d5cf92b14d45c9c9e81c99d9ef08eefa22
Nick Shvelidze <captain@pirrate.me>
2015-05-09 18:44:38 +0400

    Update deps and add Leak Canary

--- src/com/tbilisi/bus/App.java
+++ src/com/tbilisi/bus/App.java
@@ -1,11 +1,13 @@
 package com.tbilisi.bus;
 
 import android.app.Application;
+import com.squareup.leakcanary.LeakCanary;
 
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        LeakCanary.install(this);
     }
 }
