41da0851f49c43e75fab7929a3f74bed3cf3744f
Nick Shvelidze <n.shvelidze@in2circle.com>
2015-10-01 14:08:24 +0400

    Generate stop db with english names included

--- data/gen.js
+++ data/gen.js
@@ -1,18 +1,38 @@
-var fs = require('fs');
+#!/usr/bin/env node
 
-fs.readFile("stops.json", function(err, data){
-	var json_data = JSON.parse(data);
-	var out_data = [];
-	json_data.Stops.forEach(function(stop){
-		if(stop.Type != "bus") return;
+fs = require('fs'),
+  data = fs.readFileSync("stops.json"),
+  data_en = fs.readFileSync("stops-en.json"),
+  json_data = JSON.parse(data),
+  json_data_en = JSON.parse(data_en),
+  stops_map = {},
+  out_data = [];
 
-		out_data.push({
-			lat: parseFloat(stop.Lat),
-			lon: parseFloat(stop.Lon),
-			name: stop.Name,
-			id: parseInt(stop.StopId)
-		});
-	});
-	fs.writeFile("db.json", JSON.stringify(out_data));
-});
+for (var i in json_data.Stops) {
+  stops_map[json_data.Stops[i].StopId] = json_data.Stops[i];
+}
+for (var i in json_data_en.Stops) {
+  var stop = json_data_en.Stops[i];
+  if (stops_map[stop.StopId]) {
+    stops_map[stop.StopId].Name_en = stop.Name;
+  }
+}
 
+for (var i in stops_map) {
+  var stop = stops_map[i];
+
+  if (stop.Type != "bus") continue;
+
+  var name = stop.Name.replace(/ - \[[0-9]+\]/ig, "");
+  console.log(name);
+
+  out_data.push({
+    lat: parseFloat(stop.Lat),
+    lon: parseFloat(stop.Lon),
+    name: name,
+    name_en: stop.Name_en,
+    id: parseInt(stop.StopId)
+  });
+}
+
+fs.writeFileSync("db.json", JSON.stringify(out_data));
--- data/update_stops.sh
+++ data/update_stops.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 wget http://transit.ttc.com.ge/pts-portal-services/allStops -O stops.json
+wget http://transiten.ttc.com.ge/pts-portal-services/allStops -O stops-en.json
