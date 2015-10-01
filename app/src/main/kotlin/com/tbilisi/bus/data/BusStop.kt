11abe19bc418d744d1da7b886ee51e7d8fc602e6
Nick Shvelidze <n.shvelidze@in2circle.com>
2015-10-01 14:09:46 +0400

    Add BusStop model and update database

---
+++ app/src/main/kotlin/com/tbilisi/bus/data/BusStop.kt
@@ -0,0 +1,15 @@
+package com.tbilisi.bus.data
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmClass
+
+@RealmClass
+public open class BusStop(): RealmObject() {
+    @PrimaryKey
+    public open var id = 0
+    public open var name = ""
+    public open var name_en = ""
+    public open val lat = 0.0
+    public open val lon = 0.0
+}
